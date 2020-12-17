2e04495bcc030f8f9e110bd69a6f4afab9f9fdca
MohamedAbdElmawgoud <az.019098@gmail.com>
2020-12-18 01:56:33 +0200

    final

--- src/app.js
+++ src/app.js
@@ -1,29 +1,24 @@
-const express = require('express')
 const path = require('path')
+const express = require('express')
 const hbs = require('hbs')
-const request = require('request')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
-const port = process.env.port || 5000
+const port = process.env.PORT || 3000
 
-// define path  for express config
+// Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templetes/views')
-const partialsPath = path.join(__dirname, '../templetes/partials')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
-
-// setup handlebars engine and view location
+// Setup handlebars engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
-app.set('port' ,port)
 hbs.registerPartials(partialsPath)
 
-
-// setup static directory 
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
-
 
 app.get('', (req, res) => {
     res.render('index', {
@@ -53,6 +48,7 @@ app.get('/weather', (req, res) => {
             error: 'You must provide an address!'
         })
     }
+
     geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
         if (error) {
             return res.send({ error })
@@ -72,15 +68,18 @@ app.get('/weather', (req, res) => {
     })
 })
 
-
 app.get('/products', (req, res) => {
-    console.log(req.query)
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+
+    console.log(req.query.search)
     res.send({
         products: []
     })
-
 })
-
 
 app.get('/help/*', (req, res) => {
     res.render('404', {
@@ -90,12 +89,6 @@ app.get('/help/*', (req, res) => {
     })
 })
 
-
-app.get('/', function(req, res){
-    res.redirect('/weather');
- });
-
-
 app.get('*', (req, res) => {
     res.render('404', {
         title: '404',
@@ -104,8 +97,6 @@ app.get('*', (req, res) => {
     })
 })
 
-
-
-app.listen(process.env.PORT || 5000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
